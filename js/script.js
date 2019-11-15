1e302b63a008fd01d5719818fcfc37f9675e6646
AlexAriasL <alex.arias.l@gmail.com>
2019-11-15 16:23:20 +0000

    Method chaining challenge using Jquery

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Method chaining using mouseenter and mouseleave
+$(document).ready(function() {
+   $("button") .mouseenter(function() {
+      $(this).removeClass("makeRed") .addClass ("makeBorder");
+      });
+
+      $("button") .mouseleave(function() {
+         $(this).removeClass("makeBorder") .addClass("makeRed");
+         });
+   
+   }); 
